e3164a168cb2dd8e62d2846b4b02e8c9ea53450a
pandrosov <pandrosov@list.ru>
2024-02-14 22:08:00 +0300

    Add async to post validator

--- src/validators/post-validator.ts
+++ src/validators/post-validator.ts
@@ -4,8 +4,8 @@ import {BlogRepository} from "../repositories/blog-repository";
 const shortDescriptionValidation = body('shortDescription').isString().trim().isLength({min: 1, max: 100}).withMessage('Incorrect shortDescription')
 const titleValidation = body('title').isString().trim().isLength({min: 1, max: 30}).withMessage('Incorrect title')
 const contentValidation = body('content').isString().trim().isLength({min: 1, max: 1000}).withMessage('Incorrect content')
-const blogPostValidation = body('blogId').isString().trim().custom((value) => {
-    const blog = BlogRepository.getBlogById(value)
+const blogPostValidation = body('blogId').isString().trim().custom(async (value) => {
+    const blog = await BlogRepository.getBlogById(value)
 
     if(!blog)
         throw new Error("Incorrect blogId!")
